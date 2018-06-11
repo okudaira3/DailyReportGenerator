4b74f86301d2f57df65b6aec0699a91fd81cb55a
mikako-iwasaki <miwasaki@tlv.co.jp>
2018-06-11 13:06:41 +0900

    APIキーとかを外部から指定できるように変更

--- js/dailyReport.js
+++ js/dailyReport.js
@@ -22,10 +22,20 @@ var planList = [];
 /**
  *  On load, called to load the auth2 library and API client library.
  */
-function handleClientLoad() {
+function handleClientLoad(clientId,apiKey) {
 
-  CLIENT_ID = '686773253686-sfaav9jvk1i4l603k9c394d86tlorhoj.apps.googleusercontent.com';
-  API_KEY = 'AIzaSyCc_MmF8KxOgjJgkffQEhXKZiF68VPdV7Y';
+  if(!clientId){ // APIキーが指定されていない場合
+  	CLIENT_ID = '686773253686-sfaav9jvk1i4l603k9c394d86tlorhoj.apps.googleusercontent.com';
+  } else {
+  	CLIENT_ID = clientId;
+  }
+  
+  if(!apiKey){ // APIキーが指定されていない場合
+  	API_KEY = 'AIzaSyCc_MmF8KxOgjJgkffQEhXKZiF68VPdV7Y';
+  } else {
+  	API_KEY = apiKey;
+  }
+  
   // Array of API discovery doc URLs for APIs used by the quickstart
   DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
   // Authorization scopes required by the API; multiple scopes can be
